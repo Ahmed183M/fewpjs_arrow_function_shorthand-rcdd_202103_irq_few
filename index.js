e4a0aacc5c835bccb0be78ec718956bf49ee1e0a
Ahmed Mohammed <ahmed1831990@gmail.com>
2021-05-14 19:33:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,21 +1,9 @@
-const add = (num1,num2)=>{
-  return num1+num2;
+const divide = (num1,num2)=>{
+  return num1/num2;
 }
 
-const add = (num1,num2)=>{
-  return num1+num2;
-}
-
-const add = (num1,num2)=>{
-  return num1+num2;
-}
-
-const add = (num1,num2)=>{
-  return num1+num2;
-}
-
-const add = (num1,num2)=>{
-  return num1+num2;
+const square = (num1)=>{
+  return num1*num1;
 }
 
 const add = (num1,num2)=>{
